eff5cfe31d6c3d6bd363e411d61ba3406beef2d7
amitrai <aforamitrai@gmail.com>
2019-09-05 22:24:57 +0530

    list

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,4 @@
 import streams from "../apis/streams";
-import { AST_Unary } from "terser";
 
 export const signIn = userId => {
   return {
@@ -15,10 +14,15 @@ export const signOut = () => {
 };
 
 export const createStream = formValues => {
-  return async dispatch => {
-    const response = streams.post("/streams", formValues);
+  return async (dispatch, getState) => {
+    const { UserId } = getState().auth;
+    const response = await streams.post("/streams", { ...formValues, UserId });
     dispatch({ type: "CREATE_STREAM", payload: response.data });
   };
+};
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get(`/streams/`);
+  dispatch({ type: "FETCH_STREAMS", payload: response.data });
 };
 
 export const fetchStream = id => async dispatch => {
@@ -29,4 +33,9 @@ export const fetchStream = id => async dispatch => {
 export const editStream = (id, formValues) => async dispatch => {
   const response = await streams.put(`/streams/${id}`, formValues);
   dispatch({ type: "EDIT_STREAM", payload: response.data });
+};
+
+export const deleteStream = id => async dispacth => {
+  const response = await streams.delete(`/stream/${id}`);
+  dispacth({ type: "DELETE_STREAM", payload: response.data });
 };
--- src/streams/StreamList.js
+++ src/streams/StreamList.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
+import { fetchStreams } from "../actions/index";
+import { connect } from "react-redux";
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+class StreamList extends Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return <div className="right floated content">edit</div>;
+    }
+  }
+
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camara"></i>
+          <div className="content">
+            {stream.title}{" "}
+            <div className="description">{stream.descriptition}</div>
+          </div>
+          {this.renderAdmin(stream)}
+        </div>
+      );
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userID
+  };
 };
 
-export default StreamList;
+export default connect(
+  mapStateToProps,
+  { fetchStreams }
+)(StreamList);
